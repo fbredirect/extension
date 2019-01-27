fceddb3ebc595779cbf8a8e4a4ca03866e620195
Louis Heath <lheath1998@gmail.com>
2019-01-27 09:19:39 +0000

    works on page refresh

--- redirect.js
+++ redirect.js
@@ -1,1 +1,20 @@
-window.location = "http://fbredirect.github.io/";
+checkUrl(window.location.href);
+
+function checkUrl(fbUrl) {
+
+	let accepted = [
+		"/events",
+		"/messages",
+		"/groups",
+		"/pages"
+	];
+	let block = true;
+	accepted.forEach(a => {
+		if (fbUrl.includes(a))
+			block = false;
+	});
+	if (block) {
+		window.location = "http://fbredirect.github.io/";
+	}
+		
+}
